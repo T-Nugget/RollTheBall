be6fbae6297a7c49f7bd6f01ecc86850452d0b54
Travis <travismnugent@knights.ucf.edu>
2019-07-09 22:03:27 -0400

    Completed Challenge

---
+++ RollTheBall/Assets/Scripts/MovingWalls.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MovingWalls : MonoBehaviour
+{
+public float speed = 2;
+void Update()
+{
+transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time*speed, 8));
+}
+ 
+}
--- RollTheBall/Assets/Scripts/PlayerController.cs
+++ RollTheBall/Assets/Scripts/PlayerController.cs
@@ -3,42 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
     public float speed;
-    public Text countText;
+    public Text countText, livesText;
     public Text winText;
-
+     public GameObject other;
+   
     private Rigidbody rb;
-    private int count;
-    void Start () {
+    public int count;
+
+    private int life;
+  
+    void Start () 
+    {
         rb = GetComponent<Rigidbody> ();
         count = 0;
+        life = 3;
+
+        
         countText.text = "Count: " + count.ToString ();
-        if (count >= 7) {
+        if (count >= 23) 
+        {
             winText.text = "You Win!";
         }
         winText.text = "";
+
+        livesText.text = "Lives: " + life.ToString ();
+        if (life <= 0) 
+        {
+            winText.text = "You Lose!";
+            Destroy(this);
+        }
+        winText.text = "";
+
+       
+       
     }
 
-    void FixedUpdate () {
+    void FixedUpdate () 
+    {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
         if (Input.GetKey ("escape"))
             Application.Quit ();
         Vector3 Movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (Movement * speed);
+        rb.AddForce (Movement * speed);  
+
+         if (count >= 8)
+        {
+            Destroy(other);
+        }
     }
 
-    void OnTriggerEnter (Collider other) {
-        if (other.gameObject.CompareTag ("Pick Up")) {
+    
+       void OnTriggerEnter (Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pick Up")) 
+        {
             other.gameObject.SetActive (false);
             count = count + 1;
             countText.text = "Count: " + count.ToString ();
-            if (count >= 7) {
+            if (count >= 23) 
+            {
                 winText.text = "You Win!";
             }
         }
+
+        
+        if (other.gameObject.CompareTag ("Enemy")) 
+        {
+            other.gameObject.SetActive (false);
+            life = life - 1;
+            livesText.text = "Lives: " + life.ToString ();
+            if (life <= 0) 
+            {
+                winText.text = "You Lose!";
+                Destroy(this);
+            }
+        }
+        
     }
+
+
+
 
 }
